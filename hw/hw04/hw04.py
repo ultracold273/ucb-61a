2be17934c0d6cb131b79a1818a3a19c5bf5c1d7f
Wei Lx <lx@WeideMacBook-Pro.local>
2016-01-21 01:32:25 +0800

    add hw04

---
+++ hw/hw04/hw04.py
@@ -0,0 +1,155 @@
+
+def deep_len(lst):
+    """Returns the deep length of the list.
+
+    >>> deep_len([1, 2, 3])     # normal list
+    3
+    >>> x = [1, [2, 3], 4]      # deep list
+    >>> deep_len(x)
+    4
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> deep_len(x)
+    6
+    """
+    deep_length = 0;
+    for elem in lst:
+        if type(elem) == list:
+            deep_length += deep_len(elem)
+        else:
+            deep_length += 1
+    return deep_length
+
+
+def interval(a, b):
+    """Construct an interval from a to b."""
+    lower = a if a < b else b
+    upper = b if b >=a else a
+    return [lower, upper]
+
+def lower_bound(x):
+    """Return the lower bound of interval x."""
+    return x[0]
+
+def upper_bound(x):
+    """Return the upper bound of interval x."""
+    return x[1]
+
+def str_interval(x):
+    """Return a string representation of interval x.
+
+    >>> str_interval(interval(-1, 2))
+    '-1 to 2'
+    """
+    return '{0} to {1}'.format(lower_bound(x), upper_bound(x))
+
+def add_interval(x, y):
+    """Return an interval that contains the sum of any value in interval x and
+    any value in interval y.
+
+    >>> str_interval(add_interval(interval(-1, 2), interval(4, 8)))
+    '3 to 10'
+    """
+    lower = lower_bound(x) + lower_bound(y)
+    upper = upper_bound(x) + upper_bound(y)
+    return interval(lower, upper)
+
+def mul_interval(x, y):
+    """Return the interval that contains the product of any value in x and any
+    value in y.
+
+    >>> str_interval(mul_interval(interval(-1, 2), interval(4, 8)))
+    '-8 to 16'
+    """
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
+
+def div_interval(x, y):
+    """Return the interval that contains the quotient of any value in x divided by any value in y.
+
+    Division is implemented as the multiplication of x by the reciprocal of y.
+
+    >>> str_interval(div_interval(interval(-1, 2), interval(4, 8)))
+    '-0.25 to 0.5'
+    >>> str_interval(div_interval(interval(4, 8), interval(-1, 2)))
+    AssertionError
+    """
+    assert lower_bound(y) * upper_bound(y) > 0
+    reciprocal_y = interval(1/upper_bound(y), 1/lower_bound(y))
+    return mul_interval(x, reciprocal_y)
+
+def sub_interval(x, y):
+    """Return the interval that contains the difference between any value in x
+    and any value in y.
+
+    >>> str_interval(sub_interval(interval(-1, 2), interval(4, 8)))
+    '-9 to -2'
+    """
+    upper = upper_bound(x) - lower_bound(y)
+    lower = lower_bound(x) - upper_bound(y)
+    return interval(lower, upper)
+
+def par1(r1, r2):
+    return div_interval(mul_interval(r1, r2), add_interval(r1, r2))
+
+def par2(r1, r2):
+    one = interval(1, 1)
+    rep_r1 = div_interval(one, r1)
+    rep_r2 = div_interval(one, r2)
+    return div_interval(one, add_interval(rep_r1, rep_r2))
+
+# These two intervals give different results for parallel resistors:
+r1 = interval(3, 5)
+r2 = interval(1, 4)
+res1 = str_interval(par1(r1, r2))
+res2 = str_interval(par2(r1, r2))
+
+def multiple_references_explanation():
+    return """The mulitple reference problem..."""
+
+def quadratic(x, a, b, c):
+    """Return the interval that is the range of the quadratic defined by
+    coefficients a, b, and c, for domain interval x.
+
+    >>> str_interval(quadratic(interval(0, 2), -2, 3, -1))
+    '-3 to 0.125'
+    >>> str_interval(quadratic(interval(1, 3), 2, -3, 1))
+    '0 to 10'
+    """
+    def eval_quadratic(v):
+        return a * v * v + b * v + c
+
+    bound_1 = eval_quadratic(lower_bound(x))
+    bound_2 = eval_quadratic(upper_bound(x))
+    if a == 0:
+        return interval(bound_1, bound_2)
+
+    extreme_pt = -b / (2 * a)
+    extreme_val = eval_quadratic(extreme_pt)
+    if extreme_pt > lower_bound(x) and extreme_pt < upper_bound(x):
+        if a > 0:
+            upper = max(bound_1, bound_2)
+            lower = extreme_val
+        else:
+            upper = extreme_val
+            lower = min(bound_1, bound_2)
+        return interval(upper, lower)
+    else:
+        return interval(bound_1, bound_2)
+
+def polynomial(x, c):
+    """Return the interval that is the range of the polynomial defined by
+    coefficients c, for domain interval x.
+
+    >>> str_interval(polynomial(interval(0, 2), [-1, 3, -2]))
+    '-3 to 0.125'
+    >>> str_interval(polynomial(interval(1, 3), [1, -3, 2]))
+    '0 to 10'
+    >>> str_interval(polynomial(interval(0.5, 2.25), [10, 24, -6, -8, 3]))
+    '18.0 to 23.0'
+    """
+    "*** YOUR CODE HERE ***"
+
+
