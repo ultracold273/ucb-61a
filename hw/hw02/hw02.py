da0e8bf5fec579daa259ecfd083ca7683fb0472f
ultracold <w_weilix@hotmail.com>
2016-01-20 13:48:46 +0800

    fin hw2

---
+++ hw/hw02/hw02.py
@@ -0,0 +1,198 @@
+def square(x):
+    return x * x
+
+def triple(x):
+    return 3 * x
+
+def identity(x):
+    return x
+
+def increment(x):
+    return x + 1
+
+
+def piecewise(f, g, b):
+    """Returns the piecewise function h where:
+
+    h(x) = f(x) if x < b,
+           g(x) otherwise
+
+    >>> def negate(x):
+    ...     return -x
+    >>> abs_value = piecewise(negate, identity, 0)
+    >>> abs_value(6)
+    6
+    >>> abs_value(-1)
+    1
+    """
+    def p_fn(x):
+        return f(x) if x < b else g(x)
+
+    return p_fn
+
+
+def product(n, term):
+    """Return the product of the first n terms in a sequence.
+
+    n    -- a positive integer
+    term -- a function that takes one argument
+
+    >>> product(3, identity) # 1 * 2 * 3
+    6
+    >>> product(5, identity) # 1 * 2 * 3 * 4 * 5
+    120
+    >>> product(3, square)   # 1^2 * 2^2 * 3^2
+    36
+    >>> product(5, square)   # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
+    14400
+    """
+    rng_list = [term(i) for i in range(1, n + 1)]
+    prod = 1
+    for elem in rng_list:
+        prod *= elem
+    return prod
+
+def factorial(n):
+    """Return n factorial for n >= 0 by calling product.
+
+    >>> factorial(4)
+    24
+    >>> factorial(6)
+    720
+    """
+    return product(n, identity)
+
+
+from operator import add, mul
+
+def accumulate(combiner, base, n, term):
+    """Return the result of combining the first n terms in a sequence.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)   # 2 * 1^2 * 2^2 * 3^2
+    72
+    """
+    acc_list = [term(i) for i in range(1, n + 1)]
+    acc_res = base
+    for elem in acc_list:
+        acc_res = combiner(acc_res, elem)
+    return acc_res
+
+def summation_using_accumulate(n, term):
+    """An implementation of summation using accumulate.
+
+    >>> summation_using_accumulate(5, square)
+    55
+    >>> summation_using_accumulate(5, triple)
+    45
+    """
+    return accumulate(add, 0, n, term)
+
+def product_using_accumulate(n, term):
+    """An implementation of product using accumulate.
+
+    >>> product_using_accumulate(4, square)
+    576
+    >>> product_using_accumulate(6, triple)
+    524880
+    """
+    return accumulate(mul, 1, n, term)
+
+
+def repeated(f, n):
+    """Return the function that computes the nth application of f.
+
+    >>> add_three = repeated(increment, 3)
+    >>> add_three(5)
+    8
+    >>> repeated(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
+    243
+    >>> repeated(square, 2)(5) # square(square(5))
+    625
+    >>> repeated(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    """
+    if n == 0: 
+        return lambda x: x
+    elif n == 1:
+        return f
+    else:
+        return compose1(f, repeated(f, n - 1))
+    
+
+def compose1(f, g):
+    """Return a function h, such that h(x) = f(g(x))."""
+    def h(x):
+        return f(g(x))
+    return h
+
+###################
+# Extra Questions #
+###################
+
+def zero(f):
+    return lambda x: x
+
+def successor(n):
+    return lambda f: lambda x: f(n(f)(x))
+
+def one(f):
+    """Church numeral 1: same as successor(zero)"""
+    return f
+
+def two(f):
+    """Church numeral 2: same as successor(successor(zero))"""
+    return lambda x: f(f(x))
+
+three = successor(two)
+
+def church_to_int(n):
+    """Convert the Church numeral n to a Python integer.
+
+    >>> church_to_int(zero)
+    0
+    >>> church_to_int(one)
+    1
+    >>> church_to_int(two)
+    2
+    >>> church_to_int(three)
+    3
+    """
+    return n(increment)(0)
+
+def add_church(m, n):
+    """Return the Church numeral for m + n, for Church numerals m and n.
+
+    >>> church_to_int(add_church(two, three))
+    5
+    """
+    return lambda f: lambda x: m(f)(n(f)(x))
+
+def mul_church(m, n):
+    """Return the Church numeral for m * n, for Church numerals m and n.
+
+    >>> four = successor(three)
+    >>> church_to_int(mul_church(two, three))
+    6
+    >>> church_to_int(mul_church(three, four))
+    12
+    """
+    return lambda f: m(n(f))
+
+def pow_church(m, n):
+    """Return the Church numeral m ** n, for Church numerals m and n.
+
+    >>> church_to_int(pow_church(two, three))
+    8
+    >>> church_to_int(pow_church(three, two))
+    9
+    """
+    return repeated(m, church_to_int(n))
+
